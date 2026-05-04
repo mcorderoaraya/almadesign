3e1b24e56ba75b109eee8f11d52d3a56691c13dc
mcorderoaraya <mcorderoaraya@gmail.com>
2026-05-03 22:28:28 -0400

    style: update AlmaDesign color and typography tokens

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -11,7 +11,7 @@ $ogType = $ogType ?? 'website';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#111826">
+    <meta name="theme-color" content="#111827">
     <meta name="description" content="<?= e($metaDescription) ?>">
     <meta property="og:site_name" content="<?= e($siteName) ?>">
     <meta property="og:title" content="<?= e($ogTitle) ?>">
